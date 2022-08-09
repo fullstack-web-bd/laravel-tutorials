45b7450170086e8e2a48f137c7f5f7220e5493f4
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-08-09 07:47:30 +0600

    Day-1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel Basic</title>
+</head>
+
+<body class="antialiased">
+    <h2>Hello Laravel</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis expedita, aspernatur aliquid ratione
+        voluptatum quasi similique quam dicta. Animi, veritatis aut similique voluptatem inventore dolore saepe magni
+        neque reprehenderit consectetur.</p>
+</body>
+
+</html>
