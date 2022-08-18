866ff8818596cf231f8ebc32301bb1ace78a91ca
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-08-18 18:51:52 +0600

    Day03 - Laravel Views and Controller Data passing

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,10 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use stdClass;
+
 class TestController extends Controller {
     public function index()
     {
-        return view('index');
+        $name = "Akash";
+        $age = 25;
+        $users = [
+            'Akash',
+            'Jahangir',
+            'Rony'
+        ];
+
+        $user1 = new stdClass();
+        $user1->name = "Akash";
+        $user1->age = 26;
+
+        $user2 = new stdClass();
+        $user2->name = "Jahangir";
+        $user2->age = 20;
+
+        $user3 = new stdClass();
+        $user3->name = "Rony";
+        $user3->age = 35;
+
+        $users_obj = [
+           $user1,
+           $user2,
+           $user3
+        ];
+
+        // $data = [
+        //     'name' => $name,
+        //     'age' => $age,
+        // ];
+
+        return view('index', compact('name', 'age', 'users', 'users_obj'));
+        // return view('index', $data);
+        // return view('index')->with('name', $name);
+        // return view('index')->with('name', $name)->with('age', $age);
     }
 
     public function hello()
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,10 +8,49 @@
     <title>Laravel Basic</title>
 </head>
 
+@php
+    // dd($users_obj);
+@endphp
+
 <body class="antialiased">
     @include('nav')
 
     <h2>Home Page</h2>
+    <p>
+        {{ $name }}
+    </p>
+    <p>
+        {{ $age }}
+    </p>
+    <p>
+    <h4>Foreach</h4>
+    <ul>
+        @foreach ($users as $key => $user)
+            <li> {{ $key + 1 }}) {{ $user }}</li>
+        @endforeach
+
+        @foreach ($users as $user)
+            <li> {{ $loop->index + 1 }}) {{ $user }}</li>
+        @endforeach
+
+    </ul>
+
+    <h4>For</h4>
+    <ul>
+        @for ($i = 0; $i < count($users); $i++)
+            <li>{{ $users[$i] }}</li>
+        @endfor
+    </ul>
+    </p>
+
+    <h4>Users Object of Array</h4>
+    <ul>
+        @foreach ($users_obj as $user_obj)
+            <li> {{ $user_obj->name }}</li>
+            <li> {{ $user_obj->age }}</li>
+            <hr>
+        @endforeach
+    </ul>
 </body>
 
 </html>
