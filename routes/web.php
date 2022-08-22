4267ab8112372510477a14835441b9258cb52a3b
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-08-22 06:38:41 +0600

    Clear for Start class 8

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\TestController;
-use App\Http\Controllers\TodosController;
-use App\Http\Controllers\CategoriesController;
-use App\Http\Controllers\DocumentController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,41 +12,3 @@ use App\Http\Controllers\DocumentController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', [TestController::class, 'index'])->name('index');
-Route::get('/hello', [TestController::class, 'hello'])->name('tests.hello');
-Route::get('show/{id}', [TestController::class, 'show'])->name('tests.show');
-
-Route::view('/welcome', 'welcome')->name('welcome.index');
-
-// Category Create
-Route::get('/category/create', [CategoriesController::class, 'create'])->name('categories.create');
-Route::post('/category/store', [CategoriesController::class, 'store'])->name('categories.store');
-
-// Documents
-Route::resource('documents', DocumentController::class)->only([
-    'create', 'store'
-]);
-
-// Route::prefix('todos')->controller(TodosController::class)->name('todos.')->group(function () {
-//     Route::get('', 'index')->name('index');
-//     Route::get('{slug}', 'show')->name('show');
-//     Route::get('edit/{slug}', 'edit')->name('edit');
-// });
-
-Route::resource('todos', TodosController::class);
-
-// Route::redirect('/', '/todos');
-
-
-// Route::get('/portfolios', function() {
-//     return "Portfolios";
-// });
-
-// Route::get('/portfolios/1', function() {
-//     return "Portfolio Details";
-// });
-
-// Route::get('/portfolios/{id}/{slug}', function($id, $slug) {
-//     return "Portfolios:: " . $id . " SLUG::" . $slug;
-// });
